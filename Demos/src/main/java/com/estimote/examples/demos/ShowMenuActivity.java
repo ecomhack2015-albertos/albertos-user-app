1ff11ec12d658c98fdecdd25ede88e8d6f918055
Sebastian Heuer <sebastian.heuer@globetrotter.de>
2015-05-09 21:43:16 +0200

    removed demo content

--- Demos/src/main/java/com/estimote/examples/demos/ShowMenuActivity.java
+++ Demos/src/main/java/com/estimote/examples/demos/ShowMenuActivity.java
@@ -3,14 +3,8 @@ package com.estimote.examples.demos;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.GridLayout;
-import android.widget.GridView;
 import android.widget.LinearLayout;
-import android.widget.ListAdapter;
-import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
