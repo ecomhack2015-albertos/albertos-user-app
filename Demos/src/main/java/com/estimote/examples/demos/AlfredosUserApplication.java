1057d1fae340cb5a99adacf76b41927b44b98bd9
Anton <antonoehler@gmx.de>
2015-05-10 10:29:54 +0200

    Changed URL to access api on heroku (https://albertos.herokuapp.com)

--- Demos/src/main/java/com/estimote/examples/demos/AlfredosUserApplication.java
+++ Demos/src/main/java/com/estimote/examples/demos/AlfredosUserApplication.java
@@ -14,7 +14,8 @@ public class AlfredosUserApplication extends Application {
   public static Cart cart = new Cart();
 
   // TODO put this into a config file
-  public static String ApiURL = "http://10.1.227.4:9000";
+  //public static String ApiURL = "http://10.1.227.4:9000";
+  public static String ApiURL = "https://albertos.herokuapp.com";
 
   @Override
   public void onCreate() {
